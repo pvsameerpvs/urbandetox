2a659560cd4b1276f9df9ac2b3ee7a8dadeab1cb
pvsameerpvs <pvsameerpvs@gmail.com>
2026-05-10 23:45:38 +0400

    fix: vercel deplymnet image cinfig

--- apps/detox-frontend/next.config.ts
+++ apps/detox-frontend/next.config.ts
@@ -7,10 +7,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
